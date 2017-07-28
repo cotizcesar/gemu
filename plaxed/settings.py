66fda03927b4d47cbf995125199af45cc3a00826
Cesar Cotiz <cotizcesar@gmail.com>
2017-07-28 11:47:02 -0500

    Secret Key Heroku Support

--- plaxed/settings.py
+++ plaxed/settings.py
@@ -18,6 +18,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
