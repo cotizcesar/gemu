25cad5c4287b3857d75a6a3fb6b3dab51a5505bb
Cesar Cotiz <cotizcesar@gmail.com>
2017-07-28 12:23:35 -0500

    Finally! Local and Heroku is working.

--- plaxed/settings.py
+++ plaxed/settings.py
@@ -128,9 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Heroku: Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
