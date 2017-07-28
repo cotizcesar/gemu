c5404b0b0338dd9df98978d5ae31f91dec1c7180
Cesar Cotiz <cotizcesar@gmail.com>
2017-07-28 18:34:29 -0500

    Cleaning code

--- core/views.py
+++ core/views.py
@@ -3,10 +3,6 @@ from django.utils.http import is_safe_url
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
 
-# User Auth based on CBV
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
-
 # User Model Built-in Django
 from django.contrib.auth.models import User
 
@@ -56,11 +52,3 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'core/signup.html', {'form': form})
-
-class UserLogout(LogoutView):
-    next_page = 'login'
-    redirect_field_name = 'next'
-
-class UserLogin(LoginView):
-    success_url = 'timeline'
-    form_class = LoginForm
